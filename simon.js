f122e849feba9dbada92e16596da338a9eb4e81d
neechie <neechieg@gmail.com>
2017-12-14 14:18:02 -0500

    functionalify for timers working. switched to jquery from javascript

--- simon.js
+++ simon.js
@@ -14,3 +14,64 @@ var on;
 var off;
 var strict;
 var x;
+
+//Timer for how long light is lit to show user. Light will spped up after round 10
+function change1() {
+    
+    if (count <= 10) {
+        off = 400;
+        on = 800;
+    }
+    
+    else {
+        off = 250;
+        on = 500;
+    }
+    
+    //Setting X as the interval function for both the regular and strict modes. Regular = interval  Strict = Timeout
+    x = setInterval(function () {
+    //Square 1 - Green (#one - .light1 - .dark1)
+    if (random[j] == 1) {
+        lite = 'light1';
+        $('#one').addClass(lite);
+        $('#audio1')[0].play();
+        liteID.push(1);
+    
+    setTimeout(function () {
+        $('#one').removeClass(lite);}, off);
+    }
+    //Square 2 - Red (#two - .light2 - .dark2)
+        else if (random[j] == 2) {
+            lite = 'light2';
+            $('#two').addClass(lite);
+            $('#audio2')[0].play();
+            liteID.push(2);
+    
+    setTimeout(function () {
+        $('#two').removeClass(lite);}, off);
+    }
+    //Square 3 - Yellow (#three - .light3 - .dark3)
+        else if (random[j] == 3) {
+        lite = 'light3';
+        $('#three').addClass(lite);
+        $('#audio3')[0].play();
+        liteID.push(3);
+    
+    setTimeout(function () {
+        $('#three').removeClass(lite);}, off);
+    }
+    //Square 4 - Red (#four - .light4 - .dark4)
+    else {
+        lite = 'light4';
+        $('#four').addClass(lite);
+        $('#audio4')[0].play();
+        liteID.push(4);
+    
+    setTimeout(function () {
+        $('#four').removeClass(lite);}, off);
+    }
+    j++;
+    
+    if (j >= count) {clearInterval(x);}
+    }, on);
+    }
