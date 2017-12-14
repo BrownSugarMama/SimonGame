beed64895d04aa0bd85d12ff4531a150f107fe4a
neechie <neechieg@gmail.com>
2017-12-14 14:16:50 -0500

    Declared my varibles. Began setup for jquery.

---
+++ simon.js
@@ -0,0 +1,16 @@
+//Added jquery element
+$(document).ready(function () {
+    console.log('hello world')
+
+// Declaration of the varibles for my arrays
+var random = [];
+var liteID = [];
+var lite;
+var clicked = [];
+var i;
+var j = 0;
+var count = 1;
+var on;
+var off;
+var strict;
+var x;
